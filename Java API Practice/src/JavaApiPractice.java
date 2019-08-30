313177fa5da0d386e8f0824ada1437724ab87b42
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2019-08-30 10:22:32 -0600

    Java API practice initial commit

---
+++ Java API Practice/src/JavaApiPractice.java
@@ -0,0 +1,16 @@
+
+public class JavaApiPractice
+	{
+
+		public static void main(String[] args)
+			{
+				String word = ("Mullen High School"); 
+				System.out.println(word.substring(0, 3));
+				System.out.println(word.substring(7,11));
+				System.out.println(word.substring(7,11).length());
+				System.out.println(word.substring(2).indexOf("l"));
+				System.out.println(word.substring(17).indexOf("l"));
+				System.out.println(word.substring(0,7));
+			}
+
+	}
