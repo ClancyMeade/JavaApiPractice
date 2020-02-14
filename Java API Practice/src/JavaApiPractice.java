5dc94c2437e20f07f461819671d4fe211092fd79
clancy.meade <clancy.meade@mu-c1-16.mullen.pvt.k12.co.us>
2020-02-14 09:24:35 -0700

    commit

--- Java API Practice/src/JavaApiPractice.java
+++ Java API Practice/src/JavaApiPractice.java
@@ -11,7 +11,7 @@ public class JavaApiPractice
 				System.out.println(word.substring(2).indexOf("l"));
 				System.out.println(word.substring(17).indexOf("l"));
 				System.out.println(word.substring(0,6).concat(word.substring(7,11)));
-				System.out.println(word.replaceAll("e", "x"));
+				System.out.println(word.replaceAll("[e]", "x"));
 				System.out.println(word.substring(0,7).toUpperCase());
 				System.out.println(word.replaceAll(" ", ""));
 				System.out.println(word.substring(0,7).compareTo(word.substring(7,11))); 
